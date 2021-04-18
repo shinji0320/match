ebe41157e1b271e46328ab8088d80b0707a221ef
shinji0320 <shinji.blue.one@gmail.com>
2021-04-19 07:39:45 +0900

    ログイン後ページ遷移先変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-  controllers: { registrations: 'registrations' }
+  controllers: { registrations: 'registrations',  sessions: :sessions }
 
   root 'top#index'
 
